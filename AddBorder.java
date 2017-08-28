eb265fee7645539b6286c0e7427a00a53b51c0ae
Prashant Mishra <mishraprashant1811998@gmail.com>
2017-08-28 17:38:43 +0530

    Add files via upload

---
+++ AddBorder.java
@@ -0,0 +1,35 @@
+﻿/*
+
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+Example
+
+For
+
+picture = ["abc",
+           "ded"]
+the output should be
+
+addBorder(picture) = ["*****",
+                      "*abc*",
+                      "*ded*",
+                      "*****"]
+
+*/
+
+String[] addBorder(String[] picture) {
+    int n=picture.length;
+    int s=picture[0].length()+2;
+    String res[]=new String[n+2];
+    String temp="";
+    for(int i=0;i<s;i++){
+        temp+='*';
+    }
+    res[0]=temp;
+    for(int i=0;i<n;i++){
+        res[i+1]='*'+picture[i]+'*';
+    }
+    res[n+1]=temp;
+    return res;
+    
+}
